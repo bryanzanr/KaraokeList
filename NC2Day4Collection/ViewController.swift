a6a8721f55b5aa4a863de6c9e9c8c863b3ba2da3
Bryanza Novirahman <bnrvideos@live.com>
2020-03-18 23:32:49 +0700

    Add real and original music from smule

--- NC2Day4Collection/ViewController.swift
+++ NC2Day4Collection/ViewController.swift
@@ -20,21 +20,26 @@ class ViewController: UIViewController {
     let durationArr: [Int] = [5, 3, 4]
     
     var durationMusic = 0
+    @IBOutlet var leftSwipe: UISwipeGestureRecognizer!
+    @IBOutlet var rightSwipe: UISwipeGestureRecognizer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setDefault(withMusicInIndex: 0)
     }
+    @IBAction func swipeRandom(_ sender: UISwipeGestureRecognizer) {
+        switch sender {
+        case leftSwipe, rightSwipe:
+            networkData(command: "random")
+        default:
+            break
+        }
+    }
     
     @IBAction func playerButton(_ sender: UIButton) {
         switch sender{
         case shuffleButton:
-            let music = musicArr.randomElement()
-            let (cover, duration) = getCover(music: music!)
-            musicCover.image = cover
-            titleMusic.text = music
-            playListMusic.text = "Music Duration : \(duration) minute"
-            durationMusic = duration
+            networkData(command: "random")
         case libraryButton:
             performSegue(withIdentifier: "detailSegue", sender: self)
         default:
@@ -42,13 +47,38 @@ class ViewController: UIViewController {
         }
     }
     
+    func randomMusic(musicLists: [ListModel.List]?) {
+        if let music = musicLists?.randomElement() {
+            DispatchQueue.main.async {
+                self.musicCover.downloaded(from: music.coverURL)
+                self.titleMusic.text = music.title
+                self.playListMusic.text = "Total Listen : \(music.stats.totalListen) persons"
+                self.durationMusic = music.stats.totalListen
+            }
+        }
+//        let (cover, duration) = getCover(music: music!)
+//        musicCover.image = cover
+//        titleMusic.text = music
+//        playListMusic.text = "Music Duration : \(duration) minute"
+//        durationMusic = duration
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "detailSegue"{
+        switch segue.identifier {
+        case "detailSegue":
             if let dest = segue.destination as? DetailViewController {
+               dest.sendImage = musicCover.image
+               dest.titleMusic = titleMusic.text!
+               dest.durationMusic = durationMusic
+           }
+        case "searchSegue":
+            if let dest = segue.destination as? SearchViewController {
                 dest.sendImage = musicCover.image
                 dest.titleMusic = titleMusic.text!
                 dest.durationMusic = durationMusic
             }
+        default:
+            break
         }
     }
     
@@ -71,5 +101,71 @@ class ViewController: UIViewController {
         }
     }
     
+    func networkData(command: String){
+        guard let url = URL(string: "https://www.smule.com/bnrmusic/performances/json") else {return}
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            do{
+                let decoder = JSONDecoder()
+                let response = try decoder.decode(ListModel.self, from: data!)
+                switch command {
+                case "random":
+                    self.randomMusic(musicLists: response.list)
+                default:
+                    break
+                }
+             } catch let parsingError {
+                print("Error", parsingError)
+           }
+        }
+        task.resume()
+    }
+    
+}
+
+struct ListModel: Codable{
+    struct List: Codable {
+        struct Stats: Codable {
+            let totalListen: Int
+            enum CodingKeys: String, CodingKey {
+                case totalListen = "total_listens"
+            }
+        }
+        let stats: Stats
+        let title: String
+        let coverURL: String
+        enum CodingKeys: String, CodingKey {
+            case title
+            case stats
+            case coverURL = "cover_url"
+        }
+    }
+    let list: [List]
+    let nextOffset: Int
+    enum CodingKeys: String, CodingKey {
+        case list
+        case nextOffset = "next_offset"
+    }
+}
+
+extension UIImageView {
+    func downloaded(from url: URL, contentMode mode: UIView.ContentMode = .scaleAspectFit) {  // for swift 4.2 syntax just use ===> mode: UIView.ContentMode
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() {
+                self.image = image
+            }
+        }.resume()
+    }
+    func downloaded(from link: String, contentMode mode: UIView.ContentMode = .scaleAspectFit) {  // for swift 4.2 syntax just use ===> mode: UIView.ContentMode
+        guard let url = URL(string: link) else { return }
+        downloaded(from: url, contentMode: mode)
+    }
 }
 
